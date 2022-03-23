77de271a05e92a9c9aac4d59768ed0f6ab22aecd
Florian Sihler <florian.sihler@uni-ulm.de>
2022-03-23 22:11:12 +0100

    new feature: allow the automatic checking of grammar in the areas around the cursor

--- src/extension.js
+++ src/extension.js
@@ -1,8 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+const { clearInterval } = require('timers');
 const vscode = require('vscode');
 
 const duden = require("./duden")
+
+let autoCheckInterval;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -20,10 +23,10 @@ function activate(context) {
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('duden-spell-grammar-check.checkSpelling', function () {
-		// The code you place here will be executed every time your command is executed	
+		// The code you place here will be executed every time your command is executed
 		const editor = vscode.window.activeTextEditor;
 
-		if (editor) {
+		if(editor) {
 			const document = editor.document;
 			const selections = editor.selections;
 
@@ -44,11 +47,36 @@ function activate(context) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	const autoSpellCheck = function () {
+		const dudenOut = vscode.window.createOutputChannel("Duden Spell & Grammar Check");
+		const configuration = vscode.workspace.getConfiguration('duden-spell-grammar-check');
+		if(autoCheckInterval === undefined) {
+			const interval = configuration.get('autoCheckSurroundingLines.interval')
+			duden.autoSpellCheck(dudenOut);
+			dudenOut.appendLine(`Start automated checking every ${interval} milliseconds.`);
+			autoCheckInterval = setInterval(() => duden.autoSpellCheck(dudenOut), interval);
+		} else {
+			dudenOut.appendLine("Stopped automated checking.");
+			disableAutoCheck();
+		}
+	};
+	const checkSpellingAroundCursor = vscode.commands.registerCommand('duden-spell-grammar-check.checkSpellingAroundCursor', autoSpellCheck);
+	context.subscriptions.push(checkSpellingAroundCursor);
 }
+
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {
+	if(autoCheckInterval !== undefined)
+		disableAutoCheck();
+}
+
+function disableAutoCheck() {
+	clearInterval(autoCheckInterval);
+	autoCheckInterval = undefined;
+}
 
 module.exports = {
 	activate,
